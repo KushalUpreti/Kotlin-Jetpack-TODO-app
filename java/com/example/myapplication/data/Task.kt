307cbf3dacf282deeeddf14a9766551f7c0a3254
Kushal Upreti <ehunt9030@gmail.com>
2021-05-22 20:26:29 +0545

    First commit

---
+++ java/com/example/myapplication/data/Task.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "tasks")
+@Parcelize
+data class Task(
+    val checked: Boolean = false,
+    val task: String,
+    val important: Boolean = false,
+    val created: Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+) : Parcelable {
+    val createdDateFormatted: String get() = DateFormat.getDateTimeInstance().format(created)
+}
---
+++ java/com/example/myapplication/ui/tasks/TaskFragment.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class TaskFragment : Fragment(R.layout.fragment_tasks) {
+}
